08a84246baeea046f00cadf9c4384001dfa1f7c9
Marchons <d1286360646@outlook.com>
2023-06-11 14:54:42 +0800

    add LC349

--- src/lc349/C.java
+++ src/lc349/C.java
@@ -1,18 +1,12 @@
 package lc349;
 
-import java.security.cert.X509CRL;
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.HashMap;
-import java.util.List;
-
 public class C{
     public long minCost(int[] nums, int x) {
-        long res = 0;
+        long res = Long.MAX_VALUE;
         int n = nums.length;
-        long[] sum = new long[n+1];
-        for(int i =1; i <= n; i++){
-            sum[i] = i*x;
+        long[] sum = new long[n];
+        for(int i =0; i <n; i++){
+            sum[i] = (long) i*x;
         }
         for(int i = 0 ;i < n ;i++){
             int mn = nums[i];
