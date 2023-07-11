2f26b5dd7c65757ce428707da4ddd2b3a2f78331
Marchons <d1286360646@outlook.com>
2023-07-11 13:41:24 +0800

    add dp

---
+++ src/lc1600_1700/dp/MaxAlternatingSum.java
@@ -0,0 +1,12 @@
+package lc1600_1700.dp;
+
+public class MaxAlternatingSum {
+    public long maxAlternatingSum(int[] nums) {
+        long even = nums[0], odd = 0;
+        for (int i = 1; i < nums.length; i++) {
+            even = Math.max(even, odd + nums[i]);
+            odd = Math.max(odd, even - nums[i]);
+        }
+        return even;
+    }
+}
